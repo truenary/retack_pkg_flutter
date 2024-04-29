0b193787ab32f8bb015da9e2185149630e584872
Sovit <sovit.poudel@gmail.com>
2024-04-29 17:20:36 +0545

    fix: improve documentation

--- lib/retack.dart
+++ lib/retack.dart
@@ -6,12 +6,14 @@ import 'package:http/http.dart' as http;
 const String retackApiBaseUrl = 'api.retack.ai';
 const String retackApiEndpoint = '/observe/error-log/';
 
+/// Configuration for the RetackClient.
 class RetackConfig {
   final String apiKey;
 
   RetackConfig(this.apiKey);
 }
 
+/// Additional context about the user.
 class UserContext {
   // provide either unique userEmail or userName if available
   final String? userName;
@@ -32,19 +34,27 @@ class UserContext {
   }
 }
 
+/// A client for reporting errors to the Retack AI service.
 class RetackClient {
   final RetackConfig _retackConfig;
   static RetackClient? _instance;
 
-  // Private constructor to prevent instantiation from outside
+  /// Private constructor to prevent instantiation from outside.
   RetackClient._(this._retackConfig);
 
-  // Factory constructor to provide access to the singleton instance
+  /// Factory constructor to provide access to the singleton instance.
   factory RetackClient(RetackConfig retackConfig) {
     _instance ??= RetackClient._(retackConfig);
     return _instance!;
   }
 
+  /// Reports an error to the Retack AI service.
+  ///
+  /// [error] The error message.
+  /// [stackTrace] The stack trace of the error.
+  /// [userContext] Additional context about the user (optional).
+  ///
+  /// Returns `true` if the error was successfully reported, `false` otherwise.
   Future<bool> reportError(
     String error,
     dynamic stackTrace, {
