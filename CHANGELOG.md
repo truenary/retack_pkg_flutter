7bd740b3fc7fae8409eff9a64fe35755de2dacc4
BIPLAV00 <biplav.ghimire@truenary.com>
2024-09-18 09:59:53 +0545

    chore: rename apiKey to envKey and add comments for clarity

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,21 @@
+## 0.0.7
+* Changed API key variable name from `apiKey` to `envKey`.
+* Added comments for better code clarity and maintainability.
+
 ## 0.0.6
 * Added API urls as parameters
+
 ## 0.0.5
 * Updated API url for Retack AI
+
 ## 0.0.4
 * Improved Readme.MD
+
 ## 0.0.3
 * Added repository url
+
 ## 0.0.2
 * Added supported platforms
+
 ## 0.0.1
-* First SDK version of Retack.Ai with functionality to post errors with user context.
+* First SDK version of Retack.AI with functionality to post errors with user context.
--- test/retack_test.dart
+++ test/retack_test.dart
@@ -1,21 +1,30 @@
 import 'package:test/test.dart';
-
 import 'package:retack/retack.dart';
 
 void main() {
-  test('Test Retack AI fails with Invalid Key', () async {
-    final String falseenvKey = 'CMskbtq_xDA46XAkAzQe1JEY12';
-    final RetackClient client = RetackClient(RetackConfig(falseenvKey));
+  // Grouping the test case(s) under a descriptive label for better organization
+  group('RetackClient Error Reporting', () {
+    test('Test Retack AI fails with Invalid Key', () async {
+      // Arrange: Set up the RetackClient with a fake/invalid environment key.
+      final String falseEnvKey =
+          'CMskbtq_xDA46XAkAzQe1JEY12'; // This key is intentionally invalid
+      final RetackClient client = RetackClient(RetackConfig(falseEnvKey));
 
-    try {
-      throw const FormatException('Format Exception');
-    } catch (_, __) {
-      final bool resp = await client.reportError(
-        _.toString(),
-        __,
-        userContext: UserContext(userName: 'user@retack.io'),
-      );
-      expect(resp, false);
-    }
+      try {
+        // Act: Triggering an error (in this case, a FormatException) to simulate an error scenario
+        throw const FormatException('Format Exception');
+      } catch (error, stackTrace) {
+        // Act: Attempting to report the error to Retack with the invalid key
+        final bool resp = await client.reportError(
+          error.toString(), // Convert the error to a string
+          stackTrace, // Capture the stack trace of the error
+          userContext: UserContext(
+              userName: 'user@retack.io'), // Providing a mock user context
+        );
+
+        // Assert: Since the environment key is invalid, we expect the error report to fail (i.e., resp should be false)
+        expect(resp, false);
+      }
+    });
   });
 }
